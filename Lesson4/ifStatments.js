ab9a821dca2a5ca83c6b367e2bcee1987591f332
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-01-26 19:39:38 -0600

    Adding Lesson 4

---
+++ Lesson4/ifStatments.js
@@ -0,0 +1,6 @@
+i = 8; 
+
+if i =< 6
+console.log("I Feel Fantastic!!!!")
+if i >= 6
+console.log("Groggy")
---
+++ Lesson4/tipCalculator.js
@@ -0,0 +1,61 @@
+var meal = 50
+var tip = 10
+
+function tipCalculator(meal, tip) {
+// Answer
+Var tipPercentage = (meal/tip) * 100;
+}
+
+if tipPercentage < 5
+console.log("You are an awful tipper")
+
+else if { tipPercentage < 10 && tip > 20) || tip = 10
+    console.log("You are an ok tipper!");
+    }
+    
+    else if {tipPercentage < 5 && tip > 10 || tip = (meal/5)
+    console.log("You are a good tipper!");
+    }
+    
+    else if {tipPercentage > 5
+    console.log("You are a great tipper!");
+    }
+    
+    else { console.log("Thanks you for tipping!");
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // First Try
+// if tip < (meal/20)
+// console.log("You are an awful tipper")
+// }
+
+// else if {tip < (meal/10) && tip > (meal/20) || tip = (meal/10)
+// console.log("You are an ok tipper!");
+// }
+
+// else if {tip < (meal/5) && tip < (meal/10) || tip = (meal/5)
+// console.log("You are a good tipper!");
+// }
+
+// else if {tip > (meal/5)
+// console.log("You are a great tipper!");
+// }
+
+// else { console.log("Thanks you for tipping!");
+// };
