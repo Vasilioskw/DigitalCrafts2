e68f86b570f0bd18492cab828fa096d49bb4ddb8
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-01-26 20:19:21 -0600

    adding objects.js lesson

---
+++ Lesson4/arrays2.js
@@ -0,0 +1,12 @@
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+while(array.length > 0) {
+array.shift()
+
+console.log(array.length);
+}
+if {array.length <= 0 
+}
+     console.log("Done")
+};
+
---
+++ Lesson4/objects.js
@@ -0,0 +1,8 @@
+var me = {
+FirstName: "Vasilios",
+LastName: "Kambouras",
+Age: 21
+
+};
+
+console.log("Hi, my name is " + me.FirstName + " " + me.LastName + " and I am " + me.Age + ".")
