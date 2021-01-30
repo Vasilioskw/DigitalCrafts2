37ac4d9d761acce813c8ea8eb15a4b541e652028
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-01-30 17:06:57 -0600

    updating vending Machine project

--- Lesson4/VendingMachineAttempt2.js
+++ Lesson4/VendingMachineAttempt2.js
@@ -1,29 +1,66 @@
-console.log("Please enter money")
+
+
+function enterMoney(money) {
+    console.log("Please enter money");
+
+    if (money <= 0) {
+        console.log("Please try again")
+    }
+
+    else if (money > 0) {
+        console.log("You entered: "+money)
+    }
+
+    else {
+        
+        console.log("Please try again")
+    }
+
+}
+    enterMoney(5.00)
 // Assuming $5 entered
-moneyEntered = 5.00
-
-console.log("You entered: $5.00")
 
 
-var PrintVendingMachine = {
-    Snickers: "$"+1.00,
-    Gushers: "$"+0.50,
-    Gum: "$"+0.25,
-    Potato_Chips: "$"+1.50,
-    Soda: "$"+2.00,
-    Water: "$"+1.00,
-}
-/// Astetic
 
-console.log(PrintVendingMachine)
 
-var VendingMachineItems = {
-    Item1: "Snickers",
-    Item2: "Gushers",
-    Item3: "Gum",
-    Item4: "Potato_Chips",
-    Item5: "Soda",
-    Item6: "Water"
-}
-    console.log(VendingMachineItems)
-console.log("Select an item 1-6")
+
+
+
+
+
+
+
+
+
+
+
+
+// var PrintVendingMachine = {
+//     Snickers: "$"+1.00,
+//     Gushers: "$"+0.50,
+//     Gum: "$"+0.25,
+//     Potato_Chips: "$"+1.50,
+//     Soda: "$"+2.00,
+//     Water: "$"+1.00,
+// }
+// /// Astetic
+
+// console.log(PrintVendingMachine)
+// console.log("Here are items and prices")
+
+// console.log("Select items between 1-6")
+
+// var VendingMachineItems = {
+//     Item1: "Snickers",
+//     Item2: "Gushers",
+//     Item3: "Gum",
+//     Item4: "Potato_Chips",
+//     Item5: "Soda",
+//     Item6: "Water"
+// }
+//     console.log(VendingMachineItems)
+
+
+//     /// user selects Item 1 and 2
+
+//     console.log("You chose: " + VendingMachineItems.Item1 + " & " + VendingMachineItems.Item2)
