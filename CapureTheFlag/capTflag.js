59cfa93794d8e051e919239e4a48c236c1ef6b75
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-03-13 14:00:39 -0500

    API practice

---
+++ CapureTheFlag/capTflag.js
@@ -0,0 +1,12 @@
+/* Write the JS Code to add the flags to the container box and remove the flags
+    from the inline html. A flag is defined if it has a flag number id attribute */
+
+
+    var flagof1 = document.getElementById("flag1");
+var box = document.getElementById("containerBox");
+box.appendChild(flag1)
+
+    var flagof2 = document.getElementById("flag1");
+    var flagof2 = document.getElementById("flag1");
+    var flagof2 = document.getElementById("flag1");
+
