c2863047c55575ba792e164ca081f0d123b194df
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-03-18 21:33:16 -0400

    Seach bar and Joke button are working. Need to integrage bootstrap card generation with new forloop.

--- API-drinking-game/API-DG1.js
+++ API-drinking-game/API-DG1.js
@@ -1,27 +1,8 @@
 var mybutton1 = document.getElementById('press1')
+var input1 = document.getElementById('search')
 
 
-const input1 = document.getElementById('query1')
-
-mybutton1.addEventListener('click', function(event){
-    event.preventDefault()
- console.log("The term searched for was " + input1.value);
- fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+input1.value)
- .then((response) => {
-     return response.json()
-     })
-.then((data) => {
-    // console.log(data.drinks.length)
-    var drink_length = data.drinks.length
-for (let i=0; i < drink_length; i++) {
-    console.log(data.drinks[i])
-            }
-         })
-})
-
-
-
-
+// jokes
 var mybutton2 = document.getElementById('press2')
 
 mybutton2.addEventListener('click', function(event){
@@ -32,10 +13,42 @@ mybutton2.addEventListener('click', function(event){
      })
 .then((data) => {
     console.log(data)
-
-         // return data
          })
 })
+// jokes
+
+mybutton1.addEventListener('click', function(event){
+    event.preventDefault()
+ console.log("The term searched for was " + input1);
+ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+input1.value)
+ .then((response) => {
+     return response.json()
+     })
+.then((data) => {
+    // console.log(data.drinks.length)
+    var drink_length = data.drinks.length
+for (let i=0; i < drink_length; i++) {
+    console.log(data.drinks[i])
+     var drinkName = document.createElement("p")
+            drinkName.className = 'drinkcard'
+            drinkName.innerHTML = data.drinks[i].strDrink
+            document.body.appendChild(drinkName)
+            var photo = document.createElement("img")
+            photo.className = 'drinkcard'
+            photo.src = data.drinks[i].strDrinkThumb
+            document.body.appendChild(photo)
+            }
+         })
+})
+
+//
+
+
+        
+
+
+
+
 
 
 
