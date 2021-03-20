04f696716df14a0e63ee2e1aee14485a030382d7
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-03-20 14:53:57 -0400

    added a bootstrap row and col for loop to make cards on search

--- API-drinking-game/API-DG1.js
+++ API-drinking-game/API-DG1.js
@@ -1,6 +1,24 @@
 var mybutton1 = document.getElementById('press1')
 var input1 = document.getElementById('search')
 
+// ALL THE "VAR" LOGIC STUFF
+var drinkcontainer = document.createElement("div")
+    drinkcontainer.className = 'container'
+
+    var aRow = document.createElement("div")
+    aRow.className = 'row'
+   //  document.body.appendChild(aRow)
+
+
+    var aCol = document.createElement("div")
+   //  document.body.appendChild(aCol)
+
+    var drinkName = document.createElement("p")
+    var photo = document.createElement("img")
+
+
+
+
 
 // jokes
 var mybutton2 = document.getElementById('press2')
@@ -25,31 +43,47 @@ mybutton1.addEventListener('click', function(event){
      return response.json()
      })
 .then((data) => {
-    // console.log(data.drinks.length)
-    var drink_length = data.drinks.length
+ console.log(data.drinks.length)
+var drink_length = data.drinks.length 
+
+    document.body.appendChild(drinkcontainer)
+
 for (let i=0; i < drink_length; i++) {
     console.log(data.drinks[i])
-     var drinkName = document.createElement("p")
-            drinkName.className = 'drinkcard'
-            drinkName.innerHTML = data.drinks[i].strDrink
-            document.body.appendChild(drinkName)
-            var photo = document.createElement("img")
-            photo.className = 'drinkcard'
-            photo.src = data.drinks[i].strDrinkThumb
-            document.body.appendChild(photo)
-            }
-         })
+
+
+
+if (i === 0 || i % 3 === 0) {
+    document.body.appendChild(aRow)
+    aRow = document.createElement("div")
+    aRow.className = 'row'
+    document.body.appendChild(aRow)
+
+}
+ /**
+  * 1. EVERY ITERATION OF FOR LOOP, MAKE A NEW DRINK ELEMENT & NEW DRINK NAME ELEMENT
+  * 2. MAKE A NEW COLOMN 
+  * 3. ADD THE DRINK NAME & PICTURE TO THE NEW COLOMN
+  * 4. ADD THE COLOMN TO THE ROW
+  */
+
+aCol = document.createElement("div")
+aCol.className = 'col-sm'
+
+drinkName = document.createElement("p")
+drinkName.className = 'drinkcard'
+drinkName.innerHTML = data.drinks[i].strDrink
+aCol.appendChild(drinkName)
+
+photo = document.createElement("img")
+photo.className = 'drinkcard'
+photo.src = data.drinks[i].strDrinkThumb
+aCol.appendChild(photo)
+
+aRow.appendChild(aCol)
+
+
+
+}              
 })
-
-//
-
-
-        
-
-
-
-
-
-
-
-
+})
