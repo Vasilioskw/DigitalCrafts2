14a96bf5aa0ad7eb819435835ea01fd6641131c8
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-04-10 14:25:33 -0400

    More server exercises

---
+++ npmfile/catdogServer.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express');
+const app = express();
+
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send(`Hello World!`);
+});
+
+app.get('/cats', (req, res) => {
+    res.send(`Meow!`);
+});
+
+app.get('/dogs', (req, res) => {
+    res.send(`Woof!`);
+});
+
+app.get('/cats_and_dogs', (req, res) => {
+    res.send(`Dogs and cats living together...mass hysteria!!`);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
