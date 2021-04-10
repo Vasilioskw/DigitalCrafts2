376c5cbb5bd04c173db4afd9c09bf50f820bbc3c
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-04-10 10:42:28 -0400

    moving server file onto git DigitalCrafts2 repo

---
+++ npmfile/server1.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const { Server } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Request Handler
+const sever = http.createServer((req, res) => {
+console.log(req);
+
+
+//implicit header method
+res.statusCode = 200;
+res.setHeader('content-Type', 'text/plain');
+res.end('Hello World')
+})
+
+Server.listener(port, hostname, () => 
+console.log('Server is listening on http://${hostname}:${port}');
+});
