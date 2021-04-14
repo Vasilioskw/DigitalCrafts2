151bd444f46f0a059cd40f3ce2b01af9dbe2681e
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-04-13 20:49:51 -0400

    Server page done. Adding bootstrap for styling in partials file -> HTML doc

--- npmfile/expressServer/index.js
+++ npmfile/expressServer/index.js
@@ -23,14 +23,22 @@ res.render('home');
 });
 
 app.get('/friends', (req, res) => {
-    let htmlData = `<ul>`;
-    for (let friend of db) {
-        htmlData += `<li>
-                        <a href="${req.path}/${friend.name}">${friend.name}</a>
-                    </li>`;
-    }
-    htmlData += `</ul>`;
-    res.send(htmlData);
+    console.log("request path is: "+ req.path);
+    res.render('friends-list'), {
+        friends:db,
+        path:req.path
+
+}
+
+    // let htmlData = `<ul>`;
+    // for (let friend of db) {
+    //     htmlData += `<li>
+    //                     <a href="${req.path}/${friend.name}">${friend.name}</a>
+    //                 </li>`;
+    // }
+    // htmlData += `</ul>`;
+    // // res.send(htmlData);
+    // res.render('friend');
 });
 
 
@@ -38,20 +46,25 @@ app.get('/friends/:name', (req, res) => {
     console.log(req.params.name);
     var {name} = req.params;
     var friend = db.find(thisFriend => thisFriend.name === name);
+    if (friend) {
     console.log(friend);
 
-    let htmlData =``;
-    htmlData += `<h1>${friend.name}</h1>`;
-    htmlData += `<h1>${friend.skill}</h1>`;
-    htmlData += `<h1>${friend.handle}</h1>`;
+    res.render('friend', {
+        locals: {
+            friend
+        }
+    }) 
+    // let htmlData =``;
+    // htmlData += `<h1>${friend.name}</h1>`;
+    // htmlData += `<h1>${friend.skill}</h1>`;
+    // htmlData += `<h1>${friend.handle}</h1>`;
   
     res.send(htmlData);
 
-    // } 
-// else {
-// res.status(404)
-//         .send("No friend with that name found")
-//         }
+    } else {
+res.status(404)
+        .send("No friend with that name found")
+        }
 });
 
 
